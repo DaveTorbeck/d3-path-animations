e1d4d2554eb7b8cc9857516733b8b2d0b65a6fbc
David Torbeck <davetorbeck@gmail.com>
2017-01-19 10:42:28 -0800

    get dotted line draw to work

--- app.js
+++ app.js
@@ -5,9 +5,54 @@ function animateChart() {
       .interpolate("cardinal-closed");
 
   var svg = d3.select('#path-group')
-  var path = svg.select('path#path-group-two');
 
-  var d = path[0][0].attributes.d.value;
+  var pathSeven = svg.select('path#path-group-seven');
+
+  var d = pathSeven[0][0].attributes.d.value;
+
+
+  var pathSevenDotted = svg.insert('path', 'path#path-group-seven')
+    .attr('id', 'path-group-insert-two')
+    .attr('d', d)
+    .attr('stroke', '#FA2F97')
+    .attr('stroke-width', 2)
+
+  var totalLength = pathSeven.node().getTotalLength();
+
+  var dashing = '3, 3';
+  var dashLength = dashing
+                    .split(/[\s,]/)
+                    .map(function(a) { return parseFloat(a) || 0 })
+                    .reduce(function(a, b) { return a + b });
+
+  var dashCount = Math.ceil( totalLength / dashLength );
+  var newDashes = new Array(dashCount).join( dashing + " " );
+  var dashArray = newDashes + " 0, " + totalLength;
+
+  d3.select('path#path-group-insert-two')
+    .attr("stroke-dashoffset", totalLength)
+    .attr('stroke-dasharray', dashArray)
+    .transition()
+    .duration(1000)
+    .ease('linear')
+    .attr("stroke-dashoffset", 0);
+
+  function deleteDottedLine() {
+    requestAnimationFrame(deleteDottedLine)
+    pathSeven
+      .attr("stroke-dasharray", totalLength + " " + totalLength)
+      .transition()
+      .duration(500)
+      .ease('linear')
+      .attr("stroke-dashoffset", totalLength);
+  }
+
+  setTimeout(function() {
+    deleteDottedLine();
+  }, 1050)
+
+  var pathTwo = svg.select('path#path-group-two');
+  var d = pathTwo[0][0].attributes.d.value;
   // d = SmartSVGPath.reverse(d);
 
   var secondPath = svg.insert('path', 'path#path-group-three')
@@ -16,11 +61,11 @@ function animateChart() {
       .attr('stroke', '#FA2F97')
       .attr('stroke-width', 2)
 
-  var totalLength = secondPath.node().getTotalLength();
+  var totalLengthTwo = secondPath.node().getTotalLength();
 
   d3.select('path#path-group-insert-one')
-      .attr('stroke-dasharray', totalLength + " " + totalLength)
-      .attr('stroke-dashoffset', totalLength)
+      .attr('stroke-dasharray', totalLengthTwo + " " + totalLengthTwo)
+      .attr('stroke-dashoffset', totalLengthTwo)
       .transition()
       .duration(1000)
       .ease('linear')
